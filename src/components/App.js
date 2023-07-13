08cadd82008a0a29267bb863d31336620a207712
Trimbak Pakhale <118504736+Trimbak15@users.noreply.github.com>
2023-07-13 09:50:12 +0000

    second

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,44 @@
 import React, { useState, useEffect } from "react";
 import "../styles/App.css";
+import Game from "./Game";
 
 const App = () => {
-  const [renderBall, setRenderBall] = useState(false);
-  const [ballPosition, setBallPosition] = useState({ left: "0px" });
+//   const [renderBall, setRenderBall] = useState(false);
+//   const [ballPosition, setBallPosition] = useState({ left: "0px" });
 
-  const buttonClickHandler = () => {
-    setRenderBall(true);
-  };
+//   const buttonClickHandler = () => {
+//     setRenderBall(true);
+//   };
 
-  const handleKeyDown = (event) => {
-    if (event.key === "ArrowRight" && renderBall) {
-      setBallPosition((prevPosition) => ({
-        ...prevPosition,
-        left: parseInt(prevPosition.left) + 5 + "px",
-      }));
-    }
-  };
+//   const handleKeyDown = (event) => {
+//     if (event.key === "ArrowRight" && renderBall) {
+//       setBallPosition((prevPosition) => ({
+//         ...prevPosition,
+//         left: parseInt(prevPosition.left) + 5 + "px",
+//       }));
+//     }
+//   };
 
-  useEffect(() => {
-    window.addEventListener("keydown", handleKeyDown);
+//   useEffect(() => {
+//     window.addEventListener("keydown", handleKeyDown);
 
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-    };
-  }, [renderBall]);
+//     return () => {
+//       window.removeEventListener("keydown", handleKeyDown);
+//     };
+//   }, [renderBall]);
 
-  const renderBallOrButton = () => {
-    if (renderBall) {
-      return <div className="ball" style={ballPosition}></div>;
-    } else {
-      return <button onClick={buttonClickHandler}>Start</button>;
-    }
-  };
+//   const renderBallOrButton = () => {
+//     if (renderBall) {
+//       return <div className="ball" style={ballPosition}></div>;
+//     } else {
+//       return <button onClick={buttonClickHandler}>Start</button>;
+//     }
+//   };
 
-  return <div className="playground">{renderBallOrButton()}</div>;
+//   return <div className="playground">{renderBallOrButton()}</div>;
+return(
+    <Game/>
+)
 };
 
 export default App;
